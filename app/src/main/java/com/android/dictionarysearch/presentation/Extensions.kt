5f387732f2c98a4fdf832d7ddacb7ad71e04c49f
Victor <victorgevondyan@gmail.com>
2021-06-14 21:47:18 +0400

    Implement Glide.

--- app/src/main/java/com/android/dictionarysearch/presentation/Extensions.kt
+++ app/src/main/java/com/android/dictionarysearch/presentation/Extensions.kt
@@ -1,26 +1,7 @@
 package com.android.dictionarysearch.presentation
 
-import android.view.View
 import android.widget.ImageView
-import android.widget.ProgressBar
-import com.squareup.picasso.Callback
-import com.squareup.picasso.Picasso
+import com.bumptech.glide.Glide
 
-
-fun ImageView.loadImageFull(url: String?) =
-    Picasso.get().load(url).into(this)
-
-fun ImageView.loadImage(url: String, progressBar: ProgressBar) =
-    Picasso.get()
-        .load(url)
-        .placeholder(android.R.color.white)
-        .into(this, object : Callback {
-
-            override fun onError(e: java.lang.Exception?) {
-                e?.printStackTrace()
-            }
-
-            override fun onSuccess() {
-                progressBar.visibility = View.GONE
-            }
-        })
+fun ImageView.loadImage(url: String?) =
+    Glide.with(context).load(url).into(this)
